71195eed36c623540e87a53478a2f451a747b8af
Netanel Cohen <netanel153@gmail.com>
2022-07-05 16:41:30 +0300

    fix: eslint

--- tests/unit/common/utils/format.spec.ts
+++ tests/unit/common/utils/format.spec.ts
@@ -3,9 +3,9 @@ import { formatStrings, linksToString } from '../../../../src/common/utils/forma
 
 describe('format tests', () => {
   describe('linksToString tests', () => {
-    it("Should return empty link when recieving an empty array", () => {
+    it('Should return empty link when recieving an empty array', () => {
       const linksStr: Link[] = [];
-      expect(linksToString(linksStr)).toBe("");
+      expect(linksToString(linksStr)).toBe('');
     });
 
     it('Should return link as a string when recieving a single link', () => {
@@ -19,26 +19,27 @@ describe('format tests', () => {
         { name: 'testName1', description: 'testDescription1', protocol: 'testProtocol1', url: 'http://testURL1.com' },
         { name: 'testName2', description: 'testDescription2', protocol: 'testProtocol2', url: 'http://testURL2.com' },
       ];
-      const expectedResult = 'testName1,testDescription1,testProtocol1,http://testURL1.com^testName2,testDescription2,testProtocol2,http://testURL2.com';
+      const expectedResult =
+        'testName1,testDescription1,testProtocol1,http://testURL1.com^testName2,testDescription2,testProtocol2,http://testURL2.com';
       expect(linksToString(linksStr)).toStrictEqual(expectedResult);
     });
   });
 
   describe('formatStrings tests', () => {
     it("Should return object with all values without any changes if the object doesn't contain the char: '", () => {
-      const payload: Record<string, string> = {name: 'name'};
+      const payload: Record<string, string> = { name: 'name' };
       expect(formatStrings(payload)).toStrictEqual(payload);
     });
 
     it("If one key has value with the char: ', the char will be replaced by the char: `", () => {
-      const payload: Record<string, string> = {name: `na'me`};
-      const expectedResult: Record<string, string> = {name: 'na`me'};
+      const payload: Record<string, string> = { name: `na'me` };
+      const expectedResult: Record<string, string> = { name: 'na`me' };
       expect(formatStrings(payload)).toStrictEqual(expectedResult);
     });
 
     it("If multiple keys has values with the char: ', the char in all values will be replaced by the char: `", () => {
-      const payload: Record<string, string> = {name: `na'me`, name2: `name'2`};
-      const expectedResult: Record<string, string> = {name: 'na`me', name2: 'name`2'};
+      const payload: Record<string, string> = { name: `na'me`, name2: `name'2` };
+      const expectedResult: Record<string, string> = { name: 'na`me', name2: 'name`2' };
       expect(formatStrings(payload)).toStrictEqual(expectedResult);
     });
   });
